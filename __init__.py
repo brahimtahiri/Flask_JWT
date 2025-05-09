f95d43cf38ecc41dbb103a645f10b430de9055b6
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-05-09 14:43:15 +0200

    Restructuration du template.

--- __init__.py
+++ __init__.py
@@ -1,23 +1,23 @@
 from flask import Flask
-from flask import render_template
-from flask import json
 from flask import jsonify
+from flask import render_template
 from flask import request
-
+from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
 # Configuration du module JWT
 app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete"  # Ma clée privée
 jwt = JWTManager(app)
 
+
 @app.route('/')
 def hello_world():
     return render_template('accueil.html')
+
 
 # Création d'une route qui vérifie l'utilisateur et retour un Jeton JWT si ok.
 # La fonction create_access_token() est utilisée pour générer un jeton JWT.
@@ -38,6 +38,7 @@ def login():
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
-                                                                                                               
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
